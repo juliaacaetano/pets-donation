86d5324c879c7568045b82456b45da70c30e1895
juliaacaetano <juliahelena.caetano@gmail.com>
2022-07-18 00:22:05 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="indigo lighten-3"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h2 class= h2 >Pet's Donation </h2>
+        
+    
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
